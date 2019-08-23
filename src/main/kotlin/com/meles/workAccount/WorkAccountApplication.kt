12f63079eac484b2c79bb0c418aff9750913d325
Maurício Ribeiro de Melo - MML <mauricio.melo@iteris.local>
2019-08-23 16:31:24 -0300

    optimize imports

--- src/main/kotlin/com/meles/workAccount/WorkAccountApplication.kt
+++ src/main/kotlin/com/meles/workAccount/WorkAccountApplication.kt
@@ -1,12 +1,9 @@
 package com.meles.workAccount
 
-import com.meles.workAccount.config.InssConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-//@EnableConfigurationProperties(InssConfig::class)
 class WorkAccountApplication
 
 fun main(args: Array<String>) {
